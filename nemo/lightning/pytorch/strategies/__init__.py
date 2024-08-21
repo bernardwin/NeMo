6525411e5667ca052d40524d8d89c55baa2a94c2
jasonwan <jasonwan@nvidia.com>
2024-08-21 13:27:17 -0700

    add fsdp2

--- nemo/lightning/pytorch/strategies/__init__.py
+++ nemo/lightning/pytorch/strategies/__init__.py
@@ -1,8 +1,10 @@
 from nemo.lightning.pytorch.strategies.fsdp_strategy import FSDPStrategy
+from nemo.lightning.pytorch.strategies.fsdp2_strategy import FSDP2Strategy
 from nemo.lightning.pytorch.strategies.megatron_strategy import MegatronStrategy
 
 
 __all__ = [
     "FSDPStrategy",
+    "FSDP2Strategy"
     "MegatronStrategy",
 ]
